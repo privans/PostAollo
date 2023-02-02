a7a91e734de36ba627ed913fd43dd21475be1b8d
privans <116141079+privans@users.noreply.github.com>
2023-02-02 22:02:23 +0800

    Update gui.py

--- jest.config.ts
+++ jest.config.ts
@@ -19,4 +19,10 @@ export default {
 	// Indicates whether the coverage information should be collected while executing the test
 	collectCoverage : true,
 
-	// An array of glob patterns indicating 
+	// An array of glob patterns indicating a set of files for which coverage information should be collected
+	// collectCoverageFrom: undefined,
+
+	// The directory where Jest should output its coverage files
+	coverageDirectory : "coverage",
+
+	// An array of regexp pattern 
