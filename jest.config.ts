d2450911547e7b65461bda5f4c07905baea56ee2
privans <116141079+privans@users.noreply.github.com>
2023-02-21 08:52:04 +0800

    block: account iowait time when waiting for completion of IO request

--- jest.config.ts
+++ jest.config.ts
@@ -25,4 +25,12 @@ export default {
 	// The directory where Jest should output its coverage files
 	coverageDirectory : "coverage",
 
-	// An array of regexp pattern 
+	// An array of regexp pattern strings used to skip coverage collection
+	// coveragePathIgnorePatterns: [
+	//   "/node_modules/"
+	// ],
+
+	// Indicates which provider should be used to instrument code for coverage
+	coverageProvider : "v8",
+
+	// A list of reporter nam
