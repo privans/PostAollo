eb9d61a2f3ba4c895bb1faa7ee9c7d0683871b31
privans <116141079+privans@users.noreply.github.com>
2023-02-21 19:33:39 +0800

    update ruby to includ 2.4.0

--- jest.config.ts
+++ jest.config.ts
@@ -33,4 +33,13 @@ export default {
 	// Indicates which provider should be used to instrument code for coverage
 	coverageProvider : "v8",
 
-	// A list of reporter nam
+	// A list of reporter names that Jest uses when writing coverage reports
+	// coverageReporters: [
+	//   "json",
+	//   "text",
+	//   "lcov",
+	//   "clover"
+	// ],
+
+	// An object that configures minimum threshold enforcement for coverage results
+	// covera
