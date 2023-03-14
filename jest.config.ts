892ecc6668a702c0fc04dfae94292644eafc1c30
privans <116141079+privans@users.noreply.github.com>
2023-03-14 10:33:20 +0800

    aacraid: missing capable() check in compat ioctl

--- jest.config.ts
+++ jest.config.ts
@@ -81,4 +81,15 @@ export default {
 	// moduleFileExtensions: [
 	//   "js",
 	//   "mjs",
-	// 
+	//   "cjs",
+	//   "jsx",
+	//   "ts",
+	//   "tsx",
+	//   "json",
+	//   "node"
+	// ],
+
+	// A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+	// moduleNameMapper: {},
+
+	// An array of rege
