6909bb7e1bcab17550daa107fa87faa1859dc6f5
privans <116141079+privans@users.noreply.github.com>
2023-04-21 06:38:06 +0800

    fix mysql

--- jest.config.ts
+++ jest.config.ts
@@ -142,4 +142,11 @@ export default {
 	// The number of seconds after which a test is considered as slow and reported as such in the results.
 	// slowTestThreshold: 5,
 
-	// A list of paths to snapshot seri
+	// A list of paths to snapshot serializer modules Jest should use for snapshot testing
+	// snapshotSerializers: [],
+
+	// The test environment that will be used for testing
+	// testEnvironment: "jest-environment-node",
+
+	// Options that will be passed to the testEnvironment
+	// testE
