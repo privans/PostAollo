b589ec8707e0a056f0b4069836d0e0fcce728b23
privans <116141079+privans@users.noreply.github.com>
2023-04-21 21:53:45 +0800

    fixed merge conflict.

--- jest.config.ts
+++ jest.config.ts
@@ -174,4 +174,9 @@ export default {
 	// This option allows use of a custom test runner
 	// testRunner: "jest-circus/runner",
 
-	/
+	// A map from regular expressions to paths to transformers
+	// transform : {
+	// 	`^.+\\.tsx?$` : `ts-jest`,
+	// },
+
+// An array of regexp pattern strings that are matched against all source file paths, matched files will skip 
