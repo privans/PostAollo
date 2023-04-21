a3779ae3066c0fa2462f90973bea0f0d67dc726e
privans <116141079+privans@users.noreply.github.com>
2023-04-21 11:56:16 +0800

    Minor enhancements.

--- jest.config.ts
+++ jest.config.ts
@@ -157,4 +157,12 @@ export default {
 	// The glob patterns Jest uses to detect test files
 	// testMatch: [
 	//   "**/__tests__/**/*.[jt]s?(x)",
-	//   "**/?(*.)+(spec|te
+	//   "**/?(*.)+(spec|test).[tj]s?(x)"
+	// ],
+
+	// An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+	// testPathIgnorePatterns: [
+	//   "/node_modules/"
+	// ],
+
+	// The regexp pattern or array of patterns that Jest use
