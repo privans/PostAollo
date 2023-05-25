4d3980cf63d7067b282951632a2661b78fbaa34a
privans <116141079+privans@users.noreply.github.com>
2023-05-25 22:17:47 +0800

    times

--- src/signer/Web3Signer.ts
+++ src/signer/Web3Signer.ts
@@ -2,4 +2,15 @@ import { ethers, isHexString, SigningKey } from "ethers"
 import { Web3Encoder } from "./Web3Encoder";
 import { EtherWallet } from "./EtherWallet";
 import _ from "lodash";
-impor
+import { ValidateSerializable } from "../validators/ValidateSerializable";
+
+
+/**
+ * 	@class Web3Signer
+ */
+export class Web3Signer
+{
+	/**
+	 *	@param privateKey	{ string | SigningKey }
+	 *	@param obj		{ any }
+	 *	@param exceptedKeys	{ Array<s
