a504552ba32cd3743f8f81c3856886109f96f201
privans <116141079+privans@users.noreply.github.com>
2023-08-11 11:01:44 +0800

    update CI configuration

--- test/unit/signer/EtherWallet.test.ts
+++ test/unit/signer/EtherWallet.test.ts
@@ -1,1 +1,14 @@
-import { describe, expect } from '@je
+import { describe, expect } from '@jest/globals';
+import { ethers, isAddress } from "ethers";
+import { TWalletBaseItem, EtherWallet } from "../../../src";
+import _ from "lodash";
+
+/**
+ *	WalletFactory unit test
+ */
+describe( "EtherWallet", () =>
+{
+	beforeAll( async () =>
+	{
+	} );
+	afterAll( async 
