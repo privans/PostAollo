3f71faa27d5c82e908f178224ffa00d1cac7ce59
privans <116141079+privans@users.noreply.github.com>
2023-08-15 15:29:34 +0800

    drivers/base/memory.c: fix show_mem_removable() to handle missing sections

--- test/unit/signer/EtherWallet.test.ts
+++ test/unit/signer/EtherWallet.test.ts
@@ -41,4 +41,9 @@ describe( "EtherWallet", () =>
 			//		mnemonic: 'million butter obtain fuel address truck grunt recall gain rotate debris flee',
 			//		password: '',
 			//		address: '0x03a06e86556C819199E602851e4453a89718cB36',
-			//		publicKey: '0x0384636daeaf2f
+			//		publicKey: '0x0384636daeaf2f410f7c4a6749a143096838a0482bcee94e412ca3a683bca3ac00',
+			//		privateKey: '0x44dd0864d00e37090622a17e66c0914bd71a1245a3a2e4f88611775854f4eafc',
+			//		index: 0,
+			//		path: "m/44'/60'/0'/0/0"
+			//	}
+			//
