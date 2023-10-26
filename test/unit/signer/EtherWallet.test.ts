bdd555a607cebe2a6a0cf35a8efcc88ff0cd806d
privans <116141079+privans@users.noreply.github.com>
2023-10-26 20:59:16 +0800

    Added dist files

--- test/unit/signer/EtherWallet.test.ts
+++ test/unit/signer/EtherWallet.test.ts
@@ -261,4 +261,6 @@ describe( "EtherWallet", () =>
 
 		it( "should create a wallet from a private key", async () =>
 		{
-			// Creat
+			// Create a wallet from the specified private key
+			const privateKey = "0xf8ba731e3d09ce93ee6256d7393e993be01cd84de044798372c0d1a8ad9b952a";
+			const publicKey = "0x03ed2098910ab9068abd54e1562eb9dee3cb2d9fc1426df
