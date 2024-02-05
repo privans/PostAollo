44401859833b16f078db3a19ba94ce839819892b
privans <116141079+privans@users.noreply.github.com>
2024-02-06 07:51:41 +0800

    Update spanish translations

--- test/unit/signer/Web3Signer.test.ts
+++ test/unit/signer/Web3Signer.test.ts
@@ -1,1 +1,12 @@
-import { describe, expect } from '
+import { describe, expect } from '@jest/globals';
+import { EtherWallet, Web3Validator } from "../../../src";
+import { ethers, isHexString } from "ethers";
+import { Web3Signer } from "../../../src";
+import { TWalletBaseItem } from "../../../src";
+
+
+
+/**
+ *	unit test
+ */
+describe( "Signer"
