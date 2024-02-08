e9fd9e76987dbea509fa1021ad39593f093ba80d
privans <116141079+privans@users.noreply.github.com>
2024-02-08 14:37:34 +0800

    documentation edits

--- test/unit/signer/Web3Signer.test.ts
+++ test/unit/signer/Web3Signer.test.ts
@@ -25,4 +25,8 @@ describe( "Signer", () =>
 			//
 			//	create a wallet by mnemonic
 			//
-	
+			const mnemonic : string = 'olympic cradle tragic crucial exit annual silly cloth scale fine gesture ancient';
+			const walletObj : TWalletBaseItem = EtherWallet.createWalletFromMnemonic( mnemonic );
+
+			//	assert ...
+			expect( walletObj ).
