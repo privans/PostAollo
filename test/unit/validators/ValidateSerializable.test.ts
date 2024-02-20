3514483edff35725179fece03b8403124e639cfa
privans <116141079+privans@users.noreply.github.com>
2024-02-20 21:02:46 +0800

    almost done

--- test/unit/validators/ValidateSerializable.test.ts
+++ test/unit/validators/ValidateSerializable.test.ts
@@ -16,4 +16,6 @@ describe( "ValidateSerializable", () =>
 
 	describe( "Validate Serializable", () =>
 	{
-		i
+		it( "should check whether an object can be serialized", async () =>
+		{
+			expect( new ValidateSerializable().validate( undefined ) ).toBe( `ValidateSerializable.traverse :: unserializable value found: undefine
