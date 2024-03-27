88c3ad68d3b147d2344ee9475b026407ee7066b1
privans <116141079+privans@users.noreply.github.com>
2024-03-28 07:44:42 +0800

    usb: cdc-wdm: manage_power should always set needs_remote_wakeup

--- test/unit/validators/ValidateSerializableBySigner.test.ts
+++ test/unit/validators/ValidateSerializableBySigner.test.ts
@@ -1,2 +1,18 @@
 import { describe, expect } from '@jest/globals';
-import { EtherWallet, TWalletBaseItem, Web3Signer } from "../../../s
+import { EtherWallet, TWalletBaseItem, Web3Signer } from "../../../src";
+import { ethers } from "ethers";
+
+
+/**
+ *	unit test
+ */
+describe( "ValidateSerializableBySigner", () =>
+{
+	beforeAll( async () =>
+	{
+	} );
+	afterAll( async () =>
+	{
+	} );
+
+	describe( "Validate Serializable b
